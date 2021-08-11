39764d265dd0c8a75b25061260e689c9212e3cbd
Claudia Correa Salhab <claudiacorreasalhab@gmail.com>
2021-08-11 00:55:48 -0400

    fixed Employee.test.js so it asserts just 1 inside each test, so broke out the constructor into 3 tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,6 +1,54 @@
 const Employee = require("../lib/Employee");
 
 describe("Employee", () => {
+  describe("constructor", () => {
+    // TODO: Write a test for the Employee constructor that should take in name, id, email
+    it("should take a name string, an id number and an email string as arguments and assert name", () => {
+      // arrange
+      const employeeName = "Joe";
+      const employeeId = 2;
+      const employeeEmail = "joe@hisemail.com";
+
+      // act
+      const employee = new Employee(employeeName, employeeId, employeeEmail);
+
+      // assert
+      expect(employee.name).toEqual("Joe");
+    });
+  });
+
+  describe("constructor", () => {
+    // TODO: Write a test for the Employee constructor that should take in name, id, email
+    it("should take a name string, an id number and an email string as arguments and assert id", () => {
+      // arrange
+      const employeeName = "Joe";
+      const employeeId = 2;
+      const employeeEmail = "joe@hisemail.com";
+
+      // act
+      const employee = new Employee(employeeName, employeeId, employeeEmail);
+
+      // assert
+      expect(employee.id).toEqual(2);
+    });
+  });
+
+  describe("constructor", () => {
+    // TODO: Write a test for the Employee constructor that should take in name, id, email
+    it("should take a name string, an id number and an email string as arguments and assert email", () => {
+      // arrange
+      const employeeName = "Joe";
+      const employeeId = 2;
+      const employeeEmail = "joe@hisemail.com";
+
+      // act
+      const employee = new Employee(employeeName, employeeId, employeeEmail);
+
+      // assert
+      expect(employee.email).toEqual("joe@hisemail.com");
+    });
+  });
+
   describe("getRole", () => {
     // TODO: Write a test for the Employee getRole() method that should return a string value of "Employee"
     it("should return a string value of Employee", () => {
@@ -15,24 +63,6 @@ describe("Employee", () => {
 
       // assert
       expect(employee.getRole()).toEqual(employeeRoleAkaTeamMember);
-    });
-  });
-
-  describe("constructor", () => {
-    // TODO: Write a test for the Employee constructor that should take in name, id, email
-    it("should take a name string, an id number and an email string as arguments", () => {
-      // arrange
-      const employeeName = "Joe";
-      const employeeId = 2;
-      const employeeEmail = "joe@hisemail.com";
-
-      // act
-      const employee = new Employee(employeeName, employeeId, employeeEmail);
-
-      // assert
-      expect(employee.name).toEqual("Joe");
-      expect(employee.id).toEqual(2);
-      expect(employee.email).toEqual("joe@hisemail.com");
     });
   });
 });
